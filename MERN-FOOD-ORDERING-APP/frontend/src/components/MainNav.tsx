8b14b99f35e9d2590574c905d20e6914dccefaaa
MinhPhu <minhphuzcod@gmail.com>
2024-06-17 22:54:10 +0700

    Auth0 setup

--- MERN-FOOD-ORDERING-APP/frontend/src/components/MainNav.tsx
+++ MERN-FOOD-ORDERING-APP/frontend/src/components/MainNav.tsx
@@ -1,11 +1,16 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <span className="flex space-x-2 items-center">
       <Button
         variant="ghost"
         className="font-bold hover:text-blue-500 hover:bg-white"
+        onClick={async () => await loginWithRedirect()}
       >
         Log In
       </Button>
