65f8ead47d5d1caaf203b3a80952cb1fd7574b4d
MinhPhu <minhphuzcod@gmail.com>
2024-06-18 03:24:59 +0700

    Validate Access Token

--- MERN-FOOD-ORDERING-APP/frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ MERN-FOOD-ORDERING-APP/frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User, useAuth0 } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -8,19 +8,17 @@ type Props = {
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
   // const { createUser } = useCreateMyUser();
   const navigate = useNavigate();
-  const { getAccessTokenSilently } = useAuth0();
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientID = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientID || !redirectUri) {
+  if (!domain || !clientID || !redirectUri || !audience) {
     throw new Error("Unable to initiable auth");
   }
 
-  const onRedirectCallback = async (appState?: AppState, user?: User) => {
-    const token = await getAccessTokenSilently();
-    console.log("token", token);
+  const onRedirectCallback = () => {
     navigate("/auth-callback");
   };
 
@@ -30,6 +28,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientID}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
