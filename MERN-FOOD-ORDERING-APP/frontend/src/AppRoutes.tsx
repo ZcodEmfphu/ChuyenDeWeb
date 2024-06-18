4a9a5728fadec61eb84c6dbc3ca2522e01f5979b
MinhPhu <minhphuzcod@gmail.com>
2024-06-18 23:54:15 +0700

    Protected  User

--- MERN-FOOD-ORDERING-APP/frontend/src/AppRoutes.tsx
+++ MERN-FOOD-ORDERING-APP/frontend/src/AppRoutes.tsx
@@ -3,13 +3,30 @@ import Layout from "./layout/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallBackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout showHero><HomePage/></Layout>} />
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" element={<Layout><UserProfilePage/></Layout>} />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
